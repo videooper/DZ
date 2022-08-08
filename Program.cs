20c440c4875d24d6bf365f24818b1982e8d359b3
videooper <97392291+videooper@users.noreply.github.com>
2022-08-08 22:52:23 +0300

    DZ1
    DZ1

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿int a = 5;
+int b = 7;
+
+int max = a;
+if (b> max) max  = b;
+
+
+Console.WriteLine("Максимальное число");
+Console.WriteLine(max);
+
+int min = b;
+if (a<min) min = a;
+
+Console.WriteLine("Минимальное число");
+Console.WriteLine(min);
